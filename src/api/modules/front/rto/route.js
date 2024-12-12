3815ca56e020ed0cd20e69347711dadd3e0b7051
yusufChoudhary <creative.yusuf1748@gmail.com>
2024-12-12 12:03:39 +0530

    rto/list/v2

--- src/api/modules/front/rto/route.js
+++ src/api/modules/front/rto/route.js
@@ -3,5 +3,7 @@ const controller = require("./controller");
 const router = Router({ mergeParams: true });
 
 router.get('/list', controller.list);
+router.get('/list/v2', controller.lists);
+
 
 module.exports = router;
