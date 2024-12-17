5d749d44e12b87604c11d328716875314cf4eb81
yusufChoudhary <creative.yusuf1748@gmail.com>
2024-12-17 16:50:20 +0530

    promo media

--- src/api/modules/admin/promoMedia/controller.js
+++ src/api/modules/admin/promoMedia/controller.js
@@ -1,34 +1,36 @@
-const Message = require('./../../../../utilities/Message')
-const promoMedia = require('../../../../models/promoMedia')
-class controller {
+const Message = require('../../../../utilities/Message');
+const promoMedia = require('../../../../models/promoMedia');
+
+class Controller {
     static async create(req, res) {
-        // try {
-            // const response = { data: [], message: Message.noContent.message, code: Message.noContent.code, extra: {} }
-            const { name, state_name, url, description } = req.body
-            const missingFields = ["name", "state_name", "url", "description"].filter((data) => !req.body[data])
-            if (missingFields.length > 0) {
+        try {
+            const { name, state_name, url, description } = req.body;
+
+            const requiredFields = { name, state_name, url, description };
+            const missingFields = Object.keys(requiredFields).filter((key) => !requiredFields[key]);
+
+            if (missingFields.length) {
                 return res.json({
                     status_code: false,
-                    message: `Please provide ${missingFields.join(', ')}`
-                })
+                    message: `Please provide ${missingFields.join(', ')}`,
+                });
             }
-            const createdpromo = await promoMedia.create({ name, state_name, url, description })
 
-            res.json({
+            const createdPromo = await promoMedia.create(requiredFields);
+
+            return res.json({
                 status_code: true,
-                message: "Promo create succesfully",
-                data: createdpromo
-            })
+                message: 'Promo created successfully',
+                data: createdPromo,
+            });
+        } catch (error) {
+            return res.json({
+                status_code: false,
+                message: error.message || 'Internal Server Error',
+                data: [],
+            });
         }
-        // catch (e) {
-        //     res.json({
-        //         status_code: false,
-        //         message: e.message,
-        //         data: []
-        //     })
-        // }
-    // }
+    }
 }
 
-
-module.exports = controller
+module.exports = Controller;
