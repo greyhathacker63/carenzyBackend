78328ef4d8167f2f7b469f2db4ee7c85aecb2cab
yusufChoudhary <creative.yusuf1748@gmail.com>
2024-12-18 01:12:21 +0530

    change for promo

--- src/api/modules/admin/promoMedia/controller.js
+++ src/api/modules/admin/promoMedia/controller.js
@@ -31,43 +31,150 @@ class Controller {
             });
         }
     }
+
     static async list(req, res) {
         try {
             const { _id } = req.query;
-    
+
             if (!_id) {
                 return res.json({
                     status_code: false,
                     message: "Please provide _id",
-                    data:{}
+                    data: {}
                 });
             }
-    
+
             const createdPromo = await promoMedia.findById(_id);
-    
+
             if (!createdPromo) {
                 return res.json({
                     status_code: false,
                     message: "Please provide a correct ID",
-                    data:{}
+                    data: {}
                 });
             }
-    
-            return res.status(200).json({
+
+            return res.json({
                 status_code: true,
                 message: "Promo fetched successfully",
                 data: createdPromo,
             });
-    
+
         } catch (error) {
-            return res.status(500).json({
+            return res.json({
                 status_code: false,
                 message: error.message || "Internal Server Error",
-                data:{}
+                data: {}
             });
         }
     }
-    
+
+
+    static async lists(req, res) {
+        try {
+            const allPromo = await promoMedia.find({ is_deleted: false });
+
+            if (!allPromo.length) {
+                return res.json({
+                    status_code: false,
+                    message: "No promo found",
+                    data: [],
+                });
+            }
+
+            return res.json({
+                status_code: true,
+                message: "Promo fetched successfully",
+                data: allPromo,
+            });
+
+        } catch (error) {
+            return res.json({
+                status_code: false,
+                message: error.message || "Internal Server Error",
+                data: [],
+            });
+        }
+    }
+
+
+    static async editList(req, res) {
+        const { _id, name, url, state_name, description } = req.body;
+        try {
+            if (!_id) {
+                return res.json({
+                    status_code: false,
+                    message: "Please provide _id for editing",
+                });
+            }
+
+            const filter = {};
+            if (name) filter.name = name;
+            if (url) filter.url = url;
+            if (state_name) filter.state_name = state_name;
+            if (description) filter.description = description;
+
+            if (Object.keys(filter).length === 0) {
+                return res.json({
+                    status_code: false,
+                    message: "Please provide at least one field to update",
+                });
+            }
+
+            const editPromo = await promoMedia.findByIdAndUpdate(_id, { $set: filter }, { new: true });
+
+            if (!editPromo) {
+                return res.json({
+                    status_code: false,
+                    message: "No promo found with the provided _id",
+                });
+            }
+
+            return res.json({
+                status_code: true,
+                message: "Data updated successfully",
+                data: editPromo,
+            });
+        } catch (error) {
+            return res.json({
+                status_code: false,
+                message: error.message || "Internal Server Error",
+            });
+        }
+    }
+
+    static async removeList(req, res) {
+        const { _id, is_deleted } = req.body;
+        try {
+            if (!_id) {
+                return res.json({
+                    status_code: false,
+                    message: "Please provide _id for editing",
+                });
+            }
+
+            const editPromo = await promoMedia.findByIdAndUpdate(_id, { $set: { is_deleted: is_deleted } }, { new: true });
+
+            if (!editPromo) {
+                return res.json({
+                    status_code: false,
+                    message: "No promo found with the provided _id",
+                });
+            }
+
+            return res.json({
+                status_code: true,
+                message: "Data updated successfully",
+                data: editPromo,
+            });
+        } catch (error) {
+            return res.json({
+                status_code: false,
+                message: error.message || "Internal Server Error",
+            });
+        }
+    }
+
 }
 
 module.exports = Controller;
--- src/api/modules/admin/promoMedia/route.js
+++ src/api/modules/admin/promoMedia/route.js
@@ -5,5 +5,8 @@ const router = Router(); // Instantiate the router
 
 router.post('/createPromo', controller.create);
 router.get('/list',controller.list)
+router.get('/lists', controller.lists)
+router.put('/editList', controller.editList)
+router.put('/remove', controller.removeList)
 
 module.exports = router; 
