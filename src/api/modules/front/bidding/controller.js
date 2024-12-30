7ae6f5a0b8bbb8daeddc4ce31230f72e7aff4e4e
yusufChoudhary <creative.yusuf1748@gmail.com>
2024-12-30 16:15:02 +0530

    change for update form

--- src/api/modules/front/bidding/controller.js
+++ src/api/modules/front/bidding/controller.js
@@ -37,13 +37,9 @@ class bidController {
         } = req.body;
 
         const requiredFields = [
-            'customer_expectation', 'no_of_owners', 'work_needed_manual_report',
-            'state', 'rto', 'make', 'model', 'variant', 'manufacturing_date',
-            'registration_date', 'kms_driven', 'fuel_type', 'rc', 'loan',
-            'chassing_embossing', 'piece_change_if_any', 'rusting_if_any',
-            'engine_condition', 'steering_suspension', 'ac', 'tyres', 'Keys',
-            'spare_tyre', 'painted', 'insurance', 'insurance_validity',
-            'transmission_type', 'transmission'
+            'customer_expectation', 'no_of_owners', 'work_needed_manual_report', 'state', 'rto', 'make', 'model', 'variant', 'manufacturing_date',
+            'registration_date', 'kms_driven', 'fuel_type', 'rc', 'loan', 'chassing_embossing', 'piece_change_if_any', 'rusting_if_any', 'engine_condition',
+            'steering_suspension', 'ac', 'tyres', 'Keys', 'spare_tyre', 'painted', 'insurance', 'insurance_validity', 'transmission_type', 'transmission'
         ];
         const missingFields = requiredFields.filter((field) => !req.body[field]);
 
