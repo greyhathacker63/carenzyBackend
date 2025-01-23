5fba7ad4187d27aac710f79b5cac88afc1bca8a8
yusufChoudhary <creative.yusuf1748@gmail.com>
2025-01-23 13:14:32 +0530

    change for sell car

--- src/api/modules/front/sellCar/controller.js
+++ src/api/modules/front/sellCar/controller.js
@@ -1,12 +1,15 @@
 const sellCar = require('../../../../models/sellCar');
 const brandModel = require('../../../../models/brandModel')
 const modelData = require('../../../../models/modelVariant')
-const variantFeature = require('../../../../models/variantSpecFeature')
+const variantFeature = require('../../../../models/variantSpecFeature');
+const dealer = require('../../../../models/dealer')
+const mongoose = require('mongoose')
+const {ObjectId} = mongoose.Types
 
 class Controller {
     static async save(req, res) {
         try {
-            const { name, city, registration_number, make, model, variant, transmission, year, fuel, expected_price, no_of_owner,mobile } = req.body;
+            const { name, city, registration_number, make, model, variant, transmission, year, fuel, expected_price, no_of_owner,mobile, user_id } = req.body;
             const requiredFields = ["name", "expected_price", "no_of_owner", "fuel", "year", "transmission", "model", "make", "registration_number", "city","mobile"];
             const missingFields = requiredFields.filter(field => !req.body[field]);
 
@@ -45,28 +48,47 @@ class Controller {
             const errorReturn = (msg) => res.json({ status_code: false, message: msg, data: {} });
     
             if (!_id) return errorReturn("Please provide _id");
+
+
+
+            const userData = await dealer.aggregate([
+                {
+                    $match:{
+                        _id: new ObjectId(_id)
+                    }
+                },{
+                    $lookup:{
+                        from: 'brand_models',
+                        localField: '_id',
+                        foreignField: 'user_id',
+                        as: 'sellCarDetails'
+                    }
+                },{
+                    $unwind:'$sellCarDetails'
+                }
+            ])
     
-            const data = await sellCar.findById(_id);
-            if (!data) return errorReturn("Please provide a correct _id");
+            // const data = await sellCar.findById(_id);
+            // if (!data) return errorReturn("Please provide a correct _id");
     
-            const [brandModelData, modelDatas, variantFeatureData] = await Promise.all([
-                brandModel.findById(data.make),
-                modelData.findById(data.model),
-                variantFeature.findById(data.variant)
-            ]);
+            // const [brandModelData, modelDatas, variantFeatureData] = await Promise.all([
+            //     brandModel.findById(data.make),
+            //     modelData.findById(data.model),
+            //     variantFeature.findById(data.variant)
+            // ]);
     
-            if (!brandModelData) return errorReturn("Brand model does not exist");
-            if (!modelDatas) return errorReturn("Model is not found");
-            if (!variantFeatureData) return errorReturn("Variant not found");
+            // if (!brandModelData) return errorReturn("Brand model does not exist");
+            // if (!modelDatas) return errorReturn("Model is not found");
+            // if (!variantFeatureData) return errorReturn("Variant not found");
     
-            const responseData = {
-                ...data.toObject(),
-                brand_model_name: brandModelData.name,
-                model_name: modelDatas.name,
-                variant_description: variantFeatureData.description
-            };
+            // const responseData = {
+            //     ...data.toObject(),
+            //     brand_model_name: brandModelData.name,
+            //     model_name: modelDatas.name,
+            //     variant_description: variantFeatureData.description
+            // };
     
-            return res.json({ status_code: true, message: "Data found", data: responseData });
+            return res.json({ status_code: true, message: "Data found", data: userData });
     
         } catch (error) {
             return res.json({ status_code: false, message: error.message, data: [] });
