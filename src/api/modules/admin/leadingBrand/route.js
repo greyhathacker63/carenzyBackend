3e71dd50f69e651881b2205f942f0fca454285e2
yusufChoudhary <creative.yusuf1748@gmail.com>
2025-01-24 21:52:17 +0530

    change for detail

--- src/api/modules/admin/leadingBrand/route.js
+++ src/api/modules/admin/leadingBrand/route.js
@@ -7,13 +7,13 @@ const router = express.Router();
 const upload = multer({
     storage: multer.memoryStorage(),
     limits: { files: 1, fileSize: 10 * 1024 * 1024 }, // 10MB limit
-    fileFilter: (req, file, cb) => {
-        if (file.mimetype === 'image/png') {
-            cb(null, true);
-        } else {
-            cb(new Error("Only PNG files are allowed"));
-        }
-    },
+    // fileFilter: (req, file, cb) => {
+    //     if (file.mimetype === 'image/png') {
+    //         cb(null, true);
+    //     } else {
+    //         cb(new Error("Only PNG files are allowed"));
+    //     }
+    // },
 });
 
 router.post("/upload", upload.single('file'), Controller.save);
