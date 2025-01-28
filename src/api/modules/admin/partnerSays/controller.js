b6a763eff117de80afd408794b18e8dd7c914208
yusufChoudhary <creative.yusuf1748@gmail.com>
2025-01-28 12:28:59 +0530

    chnage for delete  what cutomer says

--- src/api/modules/admin/partnerSays/controller.js
+++ src/api/modules/admin/partnerSays/controller.js
@@ -34,86 +34,103 @@ class Controller {
   static async detail(req, res) {
     try {
 
-        let { page = 1, limit = 20 } = req.query;
-        page = Math.max(1, Number(page));
-        limit = Math.max(1, Number(limit));
+      let { page = 1, limit = 20 } = req.query;
+      page = Math.max(1, Number(page));
+      limit = Math.max(1, Number(limit));
 
-        const allPartnerSays = await partnerSays.aggregate([
-          {
-            $lookup:{
-              from: 'dealers',
-              localField: 'dealer_id',
-              foreignField: '_id',
-              as: 'dealerData'
-            }
-          },{
-            $unwind:'$dealerData'
-          },{
-            $project: {
-              _id:0,
-              avatar: '$dealerData.avatar',
-              name: "$dealerData.name",
-              dealer_id: 1,
-              description:1,
-              createdAt: 1
-            }
-          },{
-            $facet:{
-              paginatedData:[
-                {
-                  $sort:{
-                    updatedAt:-1
-                  },
-                },
-                {
-                  $skip: (page-1)*limit
-                },
-                {
-                  $limit: limit
-                }
-              ],
-              total:[
-                {
-                  $count: 'total'
-                }
-              ]
-            }
+      const allPartnerSays = await partnerSays.aggregate([
+        {
+          $lookup: {
+            from: 'dealers',
+            localField: 'dealer_id',
+            foreignField: '_id',
+            as: 'dealerData'
           }
-        ])
-        const data = allPartnerSays[0]?.paginatedData
-        const total = allPartnerSays[0]?.total[0]?.total
+        }, {
+          $unwind: '$dealerData'
+        }, {
+          $project: {
+            _id: 0,
+            avatar: '$dealerData.avatar',
+            name: "$dealerData.name",
+            dealer_id: 1,
+            description: 1,
+            createdAt: 1
+          }
+        }, {
+          $facet: {
+            paginatedData: [
+              {
+                $sort: {
+                  updatedAt: -1
+                },
+              },
+              {
+                $skip: (page - 1) * limit
+              },
+              {
+                $limit: limit
+              }
+            ],
+            total: [
+              {
+                $count: 'total'
+              }
+            ]
+          }
+        }
+      ])
+      const data = allPartnerSays[0]?.paginatedData
+      const total = allPartnerSays[0]?.total[0]?.total
 
 
-        res.json({
-            status_code: !!total,
-            message: total ? "Data fetched successfully" : "No data found",
-            data,
-            total: total || 0
-        });
+      res.json({
+        status_code: !!total,
+        message: total ? "Data fetched successfully" : "No data found",
+        data,
+        total: total || 0
+      });
     } catch (error) {
-        res.json({
-            status_code: false,
-            message: error.message,
-            data: []
-        });
+      res.json({
+        status_code: false,
+        message: error.message,
+        data: []
+      });
     }
-}
+  }
 
-// static async delete(req,res){
-//   const {_id}= req.query
-//   if(!_id){
-//     return res.json({
-//       status_code: false,
-//       message : "Please provide _id"
-//     })
-//   }
-//   const deleteData = await partnerSays.findByIdAndDelete({_id})
-//   if(!deleteData){
-//     return res.json({
-//       status_code: false
-//     })
-//   }
-// }
+  static async delete(req, res) {
+    const { _id } = req.query;
+    if (!_id) {
+      return res.json({
+        status: false,
+        message: "Please provide _id",
+        data: {}
+      });
+    }
+    try {
+      const deleteData = await partnerSays.findByIdAndDelete(_id);
+      if (!deleteData) {
+        return res.json({
+          status: false,
+          message: "No data found with the provided _id",
+          data: {}
+        });
+      }
+
+      return res.json({
+        status: true,
+        message: "Data deleted successfully",
+        data: {}
+      });
+    } catch (error) {
+      res.json({
+        status: false,
+        message: error.message,
+        data: {}
+      });
+    }
+  }
 
 }
 
--- src/api/modules/admin/partnerSays/route.js
+++ src/api/modules/admin/partnerSays/route.js
@@ -4,6 +4,7 @@ const controller = require('./controller')
 
 router.post('/save', controller.save)
 router.get('/detail', controller.detail)
+router.delete('/delete', controller.delete)
 
 module.exports = router
 
