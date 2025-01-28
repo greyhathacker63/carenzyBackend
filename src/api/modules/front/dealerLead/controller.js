76018e31f10b6b14758097b9ef20d333bdac67ef
yusufChoudhary <creative.yusuf1748@gmail.com>
2025-01-28 12:17:23 +0530

    change for dealer from

--- src/api/modules/front/dealerLead/controller.js
+++ src/api/modules/front/dealerLead/controller.js
@@ -2,6 +2,8 @@ const Response = require('../../../../utilities/Response');
 const Message = require('../../../../utilities/Message');
 const service = require("../../../../services/dealerCar");
 const dealerLeadServices = require('../../../../services/dealerLead');
+const dealerLeadModel = require('../../../../models/dealerLead')
+const mongoose = require('mongoose')
 
 class Controller {
     static async save(req, res) {
@@ -18,19 +20,73 @@ class Controller {
             Response.fail(res, Response.createError(Message.dataDeletionError, err));
         }
     }
+    // static async list(req, res) {
+    //     try {
+    //         const response = { data: [], message: Message.noContent.message, code: Message.noContent.code, extra: {} };
+    //         const srvRes = await dealerLeadServices.listFront({ ...req.query, dealerToId11: req.__cuser._id });
+    //         if (srvRes.data.length) {
+    //             response.data = srvRes.data;
+    //             response.message = Message.dataFound.message;
+    //             response.code = Message.dataFound.code;
+    //         }
+    //         response.extra = srvRes.extra;
+    //         Response.success(res, response);
+    //     } catch (err) {
+    //         Response.fail(res, Response.createError(Message.dataFetchingError, err));
+    //     }
+    // }
+
     static async list(req, res) {
         try {
-            const response = { data: [], message: Message.noContent.message, code: Message.noContent.code, extra: {} };
-            const srvRes = await dealerLeadServices.listFront({ ...req.query, dealerToId11: req.__cuser._id });
-            if (srvRes.data.length) {
-                response.data = srvRes.data;
-                response.message = Message.dataFound.message;
-                response.code = Message.dataFound.code;
+            let { dealerFromId, page = 1, limit = 20 } = req.query;
+            page = Math.max(1, Number(page));
+            limit = Math.max(1, Number(limit));
+
+            if (!dealerFromId) {
+                return res.json({
+                    status_code: false,
+                    message: "Please provide dealerFromId",
+                    data: [],
+                    total: 0
+                });
             }
-            response.extra = srvRes.extra;
-            Response.success(res, response);
+
+            const dealerFromIdData = await dealerLeadModel.aggregate([
+                {
+                    $match: {
+                        dealerFromId: new mongoose.Types.ObjectId(dealerFromId)
+                    }
+                },
+                {
+                    $facet: {
+                        paginatedData: [
+                            { $sort: { createdAt: -1 } },
+                            { $skip: (page - 1) * limit },
+                            { $limit: limit }
+                        ],
+                        total: [
+                            { $count: 'total' }
+                        ]
+                    }
+                }
+            ]);
+
+            const data = dealerFromIdData[0]?.paginatedData || [];
+            const total = dealerFromIdData[0]?.total[0]?.total || 0;
+
+            res.json({
+                status_code: true,
+                message: "Data fetched successfully",
+                data: data,
+                total: total
+            });
         } catch (err) {
-            Response.fail(res, Response.createError(Message.dataFetchingError, err));
+            res.json({
+                status_code: false,
+                message: err.message,
+                data: [],
+                total: 0
+            });
         }
     }
 }
